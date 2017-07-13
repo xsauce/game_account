f81288c12bc6716f22c8713955823d20452d2ae7
guxin <guxin01@chinatopcredit.com>
2017-07-13 18:24:57 +0800

    improve account

--- incr_sql/2017-07-12.sql
+++ incr_sql/2017-07-12.sql
@@ -1,3 +1,21 @@
+CREATE TABLE `game_user_priv` (
+  `pkid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_pkid` int(10) unsigned NOT NULL,
+  `priv_item` varchar(255) NOT NULL,
+  `game_type_pkid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`pkid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `game_type` (
+  `pkid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type_name` varchar(255) NOT NULL,
+  `type_desc` varchar(255) DEFAULT NULL,
+  `most_player_count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`pkid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 ALTER TABLE game_account.game_user ADD expired_at DATETIME DEFAULT '2199-01-01 00:00:00' NOT NULL ;
 INSERT INTO `game_type` (`pkid`, `created_at`, `updated_at`, `type_name`, `type_desc`) VALUES (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'mj-baida', '麻将BD'), (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'pk-pinshi', '扑克PS');
 INSERT INTO `game_user_priv` (`pkid`, `created_at`, `updated_at`, `user_pkid`, `priv_item`, `game_type_pkid`) VALUES (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', 'all', '1'), (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', 'all', '2');
@@ -11,4 +29,5 @@ UPDATE `game_result` SET user_pkid=1,game_type_pkid=1;
 UPDATE `game_close_bill_history_game` SET user_pkid=1,game_type_pkid=1;
 UPDATE `game_close_bill_history_detail` SET user_pkid=1,game_type_pkid=1;
 UPDATE `game_close_bill_history` SET user_pkid=1,game_type_pkid=1;
-ALTER TABLE game_account.game_type ADD most_player_count INT DEFAULT 0 NOT NULL ;
+
+
